f4fd2da93c89b98ae0ad090bda353c7cf00df6dd
Jayanring <jayanring716@gmail.com>
2023-12-08 16:25:14 +0800

    a

--- node_scripts/yamls/backup/update_backup_yamls.py
+++ node_scripts/yamls/backup/update_backup_yamls.py
@@ -12,7 +12,7 @@ BACKUP_NODE = os.getenv("BACKUP_NODE")
 ARGS = os.getenv("ARGS")
 
 # backup_pvc
-with open("./yamls/backup_pvc.yaml", "r") as stream:
+with open("./yamls/backup/backup_pvc.yaml", "r") as stream:
     try:
         yaml_data = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
@@ -20,7 +20,7 @@ with open("./yamls/backup_pvc.yaml", "r") as stream:
 
 yaml_data["spec"]["storageClassName"] = SHARE_SC
 
-with open("./yamls/backup_pvc.yaml", "w") as outfile:
+with open("./yamls/backup/backup_pvc.yaml", "w") as outfile:
     try:
         yaml.safe_dump(yaml_data, outfile, default_flow_style=False)
     except yaml.YAMLError as exc:
@@ -28,7 +28,7 @@ with open("./yamls/backup_pvc.yaml", "w") as outfile:
 
 
 # backup_job
-with open("./yamls/backup_job.yaml", "r") as stream:
+with open("./yamls/backup/backup_job.yaml", "r") as stream:
     try:
         yaml_data = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
@@ -49,7 +49,7 @@ yaml_data["spec"]["template"]["spec"]["volumes"][2]["configMap"]["name"] = (
     STS_NAME + "-config"
 )
 
-with open("./yamls/backup_job.yaml", "w") as outfile:
+with open("./yamls/backup/backup_job.yaml", "w") as outfile:
     try:
         yaml.safe_dump(yaml_data, outfile, default_flow_style=False)
     except yaml.YAMLError as exc:
